3ad4bf02e854a49b9382d561ab82696b8a8fd2b1
Rakesh Mishra <jrakesh173@gmail.com>
2020-08-23 17:07:35 +0530

    Blog order by fixed

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    all_blogs = Blog.objects.order_by('created_ts')
+    all_blogs = Blog.objects.order_by('-created_ts')
     return render(request, 'blog/all_blogs.html', {'all_blogs':all_blogs}) 
 
 def detail(request, blog_id):
